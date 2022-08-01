244df805ef0124668fd331354e5917f25960836f
Gaurav Yadav <gk5480325@gmail.com>
2022-08-01 15:12:52 +0530

    reducers section over middleware started

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import configureStore from "./store/configureStore";
-import {bugAdded, bugResolved, getUnresolvedBugs} from './store/bugs';
+import {bugAdded, bugResolved,bugAssignedToUser, getUnresolvedBugs, getBugsByUser } from './store/bugs';
 import { projectAdded } from "./store/projects";
+import { userAdded } from "./store/users";
 
 const store = configureStore();
 
@@ -8,15 +9,15 @@ store.subscribe(() => {
     console.log('Store Changed!');
 });
 
+store.dispatch(userAdded({name: "User 1"}));
+store.dispatch(userAdded({name: "User 2"}));
 store.dispatch(projectAdded({ name: "Project 1"}));
 store.dispatch(bugAdded({description: "Bug 1"}));
 store.dispatch(bugAdded({description: "Bug 2"}));
 store.dispatch(bugAdded({description: "Bug 3"}));
+store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1}));
 store.dispatch(bugResolved({ id: 1 }));
 
-
-const x = getUnresolvedBugs(store.getState());
-const y = getUnresolvedBugs(store.getState());
-
-console.log(x === y);
+const bugs = getBugsByUser(2) (store.getState());
+console.log(bugs);
 
