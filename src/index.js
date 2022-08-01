bed5901fc36ea9ea01d43dd0ac2a0c8e6b529a3b
Gaurav Yadav <gk5480325@gmail.com>
2022-08-01 15:33:14 +0530

    Logger Completed

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ store.subscribe(() => {
 });
 
 store.dispatch(userAdded({name: "User 1"}));
-store.dispatch(userAdded({name: "User 2"}));
-store.dispatch(projectAdded({ name: "Project 1"}));
-store.dispatch(bugAdded({description: "Bug 1"}));
-store.dispatch(bugAdded({description: "Bug 2"}));
-store.dispatch(bugAdded({description: "Bug 3"}));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1}));
-store.dispatch(bugResolved({ id: 1 }));
+//store.dispatch(userAdded({name: "User 2"}));
+//store.dispatch(projectAdded({ name: "Project 1"}));
+//store.dispatch(bugAdded({description: "Bug 1"}));
+//store.dispatch(bugAdded({description: "Bug 2"}));
+//store.dispatch(bugAdded({description: "Bug 3"}));
+//store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1}));
+//store.dispatch(bugResolved({ id: 1 }));
 
 const bugs = getBugsByUser(2) (store.getState());
 console.log(bugs);
